972960bab68f24163b55aba5a236549334981095
Preston Neal <mrprestonneal@gmail.com>
2023-05-28 14:34:31 -0700

    move ex1-3 to own file

--- c1/ex1-3.c
+++ c1/ex1-3.c
@@ -8,6 +8,8 @@ main()
     step = 20;
 
     fahr = lower;
+    printf("%s\t%s\n", "Fahr.", "Cels");
+    printf("===============\n");
     while (fahr <= upper) {
         celsius = (5.0/9.0) * (fahr-32.0);
         /* %4.0 -> at least 4 chars, no decimals. %6.1 -> at least 6 chars, one decimals */
