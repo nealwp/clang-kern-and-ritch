c2701df7a753a1b37c04464665e56817332d311b
Preston Neal <mrprestonneal@gmail.com>
2023-05-28 14:50:14 -0700

    use constants

--- c1/fahren-cels-table.c
+++ c1/fahren-cels-table.c
@@ -1,17 +1,16 @@
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 main()
 {
-    int lower, upper, step;
     float fahr, celsius;
 
-    lower = 0;
-    upper = 300;
-    step = 20;
-
-    fahr = lower;
-    while (fahr <= upper) {
+    fahr = LOWER;
+    while (fahr <= UPPER) {
         celsius = (5.0/9.0) * (fahr-32.0);
         /* %4.0 -> at least 4 chars, no decimals. %6.1 -> at least 6 chars, one decimals */
         printf("%4.0f %6.1f\n", fahr, celsius);
-        fahr = fahr + step;
+        fahr = fahr + STEP;
     }
 }
