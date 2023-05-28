9e6a9a6acb35fcbccf71f427657198503053d5a4
Preston Neal <mrprestonneal@gmail.com>
2023-05-28 14:48:05 -0700

    ex1-5

---
+++ c1/ex1-5.c
@@ -0,0 +1,17 @@
+main()
+{
+    int lower, upper, step;
+    float fahr, celsius;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    celsius = upper;
+    while (celsius >= lower) {
+        fahr = (celsius * (9.0/5.0)) + 32; 
+        /* %4.0 -> at least 4 chars, no decimals. %6.1 -> at least 6 chars, one decimals */
+        printf("%4.0f %6.1f\n", celsius, fahr);
+        celsius = celsius - step;
+    }
+}
