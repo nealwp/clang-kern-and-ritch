080ff2d563a10381428a3bddf81a55bc27f1e448
Preston Neal <mrprestonneal@gmail.com>
2023-06-04 14:07:30 -0700

    count digits

---
+++ c1/ex1-10.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+// print words from input, one per line
+#define YES 1
+#define NO  0
+
+int main()
+{
+    int c, inword;
+
+    inword = NO;
+    while((c = getchar()) != EOF) {
+        if (c == '\n') {
+            printf("%c", c); 
+        }
+        if (c == ' ' || c == '\n' || c == '\t') {
+            inword = NO;
+        } else if (inword == NO) {
+            inword = YES;
+            printf("%c", c);
+        }
+    }
+}
---
+++ c1/ex1-9.c
@@ -0,0 +1,4 @@
+/*
+ * i guess the boundaries would be the max size of an int? 
+ * i tested by piping testfile into the binary
+ */
