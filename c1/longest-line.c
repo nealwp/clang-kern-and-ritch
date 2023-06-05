adbd1d32530a9130ce45fba7d08c6773fe1cee11
Preston Neal <mrprestonneal@gmail.com>
2023-06-04 22:44:20 -0700

    longest line

---
+++ c1/longest-line.c
@@ -0,0 +1,49 @@
+#define MAXLINE 1000
+#define EOF -1
+
+int getline(s, lim)
+char s[];
+int lim;
+{
+    int c, i;
+
+    for (i = 0; i < lim - 1 && (c = getchar()) != EOF && c != '\n'; ++i) {
+        s[i] = c;
+    }
+    if (c == '\n') {
+        s[i] = c;
+        ++i;
+    }
+    s[i] = '\0';
+    return(i);
+}
+
+
+void copy(s1, s2)
+char s1[], s2[];
+{
+    int i;
+
+    i = 0;
+    while ((s2[i] = s1[i]) != '\0') {
+        ++i;
+    }
+}
+
+int main()
+{
+    int len, max;
+    char line[MAXLINE], save[MAXLINE];
+
+    max = 0;
+    while ((len = getline(line, MAXLINE)) > 0) {
+        if (len > max) {
+            max = len;
+            copy(line, save);
+        }
+    }
+
+    if (max > 0) {
+        printf("%s", save);
+    }
+}
